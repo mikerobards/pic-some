c03f87dd06260a60d00d797b73c271e69257c90b
Michael Robards <mikerobards@icloud.com>
2021-10-19 16:58:37 -0400

    display images

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({ children }) {
     const [allPhotos, setAllPhotos] = useState([])
+
+    const url =
+        "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        // Get the data from the api
+        // save the data to state
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+
+    console.log(allPhotos)
 
     return (
         <Context.Provider value={{ allPhotos }}>
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,7 +1,23 @@
+import React, { useContext } from "react"
+import Image from "../components/Image"
+import { Context } from "../Context"
+import { getClass } from "../utils"
+
+
 function Photos() {
+    // getting the list of allPhotos from Context
+    const { allPhotos } = useContext(Context)
+
+    // mapping over them and creating an img component
+    // the part that makes the image work is by passing the entire imgage object down
+    // through a prop called img
+    const imageElements = allPhotos.map((img, i) => (
+        <Image key={img.id} img={img} className={getClass(i)} />
+    ))
+
     return (
         <main className="photos">
-            <h1>Photos go here</h1>
+            {imageElements}
         </main>
     )
 }
